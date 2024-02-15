b7ac63ca6b4086cd3e60f4fda70d636b52da7ba8
lytiy2024 <159550078+lytiy2024@users.noreply.github.com>
2024-02-16 01:11:37 +0200

    Update utils.py
    edit

--- utils.py
+++ utils.py
@@ -11,13 +11,3 @@ def factorial(x):
         # recursive call to the function
         return (x * factorial(x-1))
 
-
-# change the value for a different result
-num = 7
-
-# to take input from the user
-# num = int(input("Enter a number: "))
-
-# call the factorial function
-result = factorial(num)
-print("The factorial of", num, "is", result)

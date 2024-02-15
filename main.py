3d010a1cecfab22045af76d89107b146abaa9257
lytiy2024 <159550078+lytiy2024@users.noreply.github.com>
2024-02-16 01:12:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# change the value for a different result
+num = 7
+
+# to take input from the user
+# num = int(input("Enter a number: "))
+
+# call the factorial function
+result = factorial(num)
+print("The factorial of", num, "is", result)

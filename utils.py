e38c8b27c22eaec769997feaa041a273435c8560
olkam2022 <117659511+olkam2022@users.noreply.github.com>
2024-02-16 02:08:57 +0200

    Update utils.py
    я поправив

--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def factorial(x):
         # recursive call to the function
         return (x * factorial(x-1))
 
+def pdv():
+    """Функція рахує суму з ПДВ та без ПДВ"""
+    x2 = return_selection * 6
+    y2 = return_selection * 6 - return_selection
+    print('Сума ПДВ ->', return_selection)
+    print('Сума з ПДВ ->', x2, 'грн')
+    print('Сума без ПДВ ->', y2, 'грн')

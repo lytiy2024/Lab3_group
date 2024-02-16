bdd4ac53d20c5f5f28d686f5749cf4295cac6015
olkam2022 <117659511+olkam2022@users.noreply.github.com>
2024-02-16 02:10:49 +0200

    Update main.py
    теж поправив

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ num = 7
 # call the factorial function
 result = factorial(num)
 print("The factorial of", num, "is", result)
+
+
+choice = int(input(colored(color='blue', attrs=['underline'], text='Оберіть операцію :')))
+return_selection = int(input(colored(color='blue', attrs=['underline'], text='Введіть суму :')))
+
+if choice == 1:
+    z_pdv()
+elif choice == 2:
+    bez_pdv()
+elif choice == 3:
+    pdv()
+elif choice > 3:
+    print('невірно.')
